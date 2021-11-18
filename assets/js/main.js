b57bcf39ce950dcfc0dac9403ec898c8d8649276
Clems-web <clement.servais1@gmail.com>
2021-11-18 16:39:48 +0100

    done creation of element if there is data in localstorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,22 +9,17 @@ let button = document.getElementById('addTask');
 let task = document.getElementById('taskName');
 
 let storage = window.localStorage;
-let indexStorage = storage.length;
+let indexStorage = storage.length + 1;
 console.log(indexStorage);
+console.log(storage);
 
-button.addEventListener('click', function () {
+for (let x = 0; x < storage.length; x++){
 
     let divSpan = document.createElement('div');
     let span = document.createElement('span');
-    span.setAttribute('data-id', indexStorage);
-    span.innerHTML = task.value;
+    span.setAttribute('data-id', storage.key(x));
+    span.innerHTML = storage.getItem(storage.key(x));
     divSpan.append(span);
-
-    storage.setItem(indexStorage, span.innerHTML);
-    indexStorage++;
-    console.log(indexStorage);
-
-    task.value = "";
 
     let divIconValide = document.createElement('div');
     let divIconEdit = document.createElement('div');
@@ -82,7 +77,84 @@ button.addEventListener('click', function () {
     taskDiv.setAttribute('class', 'taskDiv');
     taskDiv.append(divSpan, divIcon);
 
-    list.append(taskDiv);
+    list.prepend(taskDiv);
+}
+
+
+
+button.addEventListener('click', function () {
+
+    if (task.value !== "") {
+        let divSpan = document.createElement('div');
+        let span = document.createElement('span');
+        span.setAttribute('data-id', indexStorage);
+        span.innerHTML = task.value;
+        divSpan.append(span);
+
+        storage.setItem(indexStorage, span.innerHTML);
+        indexStorage++;
+        console.log(indexStorage);
+
+        task.value = "";
+
+        let divIconValide = document.createElement('div');
+        let divIconEdit = document.createElement('div');
+        let divIconDel = document.createElement('div');
+
+        divIconValide.append(new valide().getNewElement());
+        divIconEdit.append(new edit().getNewElement());
+        divIconDel.append(new del().getNewElement());
+
+        divIconDel.addEventListener('click', function () {
+            this.parentElement.parentElement.remove();
+            let spanToDel = this.parentElement.parentElement.firstChild.firstChild;
+            let dataId = spanToDel.getAttribute('data-id');
+            storage.removeItem(dataId);
+
+        });
+
+        divIconEdit.addEventListener('click', function () {
+            let divSpan = this.parentElement.parentElement.firstChild;
+            let spanToChange = this.parentElement.parentElement.firstChild.firstChild;
+
+            let textSpan = spanToChange.innerHTML;
+            spanToChange.remove();
+
+            let input = document.createElement('input');
+            input.value = textSpan;
+
+            let confirm = document.createElement('button');
+            confirm.innerHTML = 'confirm';
+
+            divSpan.append(input, confirm);
+
+            confirm.addEventListener('click', function () {
+                let newSpan = document.createElement('span');
+                newSpan.innerHTML = input.value;
+
+                divSpan.append(newSpan);
+
+                input.remove();
+                this.remove();
+            });
+
+        });
+
+        divIconValide.addEventListener('click', function () {
+            let spanTovalidate = this.parentElement.parentElement.firstChild.firstChild;
+            spanTovalidate.style.textDecoration = 'line-through';
+        });
+
+        let divIcon = document.createElement('div');
+        divIcon.setAttribute('class', 'divIcon');
+        divIcon.append(divIconValide, divIconEdit,divIconDel);
+
+        let taskDiv = document.createElement('div');
+        taskDiv.setAttribute('class', 'taskDiv');
+        taskDiv.append(divSpan, divIcon);
+
+        list.append(taskDiv);
+    }
 })
 
 
